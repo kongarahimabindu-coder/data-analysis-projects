327e5527ffc94c789b0b9d053c34ce4c0823701b
himabindu <kongarahimabindu@gmail.com>
2025-11-09 09:45:51 -0600

    commit

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -7,10 +7,30 @@ strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
 
+for i, list in enumerate(strings, start=1):
+    if "," in list:
+        delimiter = "comma"
+    elif ";" in list:
+        delimiter = "semicolon"
+    else:
+        delimiter = "space"
+    print(f"proto_list{i} uses {delimiter} as the separator")
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
 
 
+    word_list = list.split(delimiter)
+    
+    for j in range(len(word_list)):
+        word_list[j] = word_list[j]
+    
+    if delimiter == ",":
+        word_list.reverse()
+        new_string = ",".join(word_list)
+        print(f"Original string: {list}")
+        print(f"Reversed string: {new_string}\n")
+    else:
+        print(f"{list} uses {delimiter}, no reversal applied\n")
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
 
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -3,13 +3,30 @@ my_string = "LaunchCode"
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
 
-
+converted = my_string[3:] + my_string[:3]
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
 
-
+print(f"The original string was '{my_string}', and the modified string is '{converted}'")
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+actual = input("Enter a string: ") 
+num_letters = 4
+converted = actual[num_letters:] + actual[:num_letters]
+print(f"The original string was '{actual}', and the modified string is '{converted}'")
+
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+actual = input("Enter a string: ") # user input
+num_letters = 6
+
+error_message = ""
+if num_letters > len(actual):
+    error_message = f"(input too long, defaulting to 3 letters)"
+    num_letters = 3
+
+converted = actual[num_letters:] + actual[:num_letters]
+print(f"The original string was '{actual}', and the modified string is '{converted}'")
+
