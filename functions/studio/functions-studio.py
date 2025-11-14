ab1d3358a0ad0a6c570f9c5e7a65fd8253c91d78
himabindu <kongarahimabindu@gmail.com>
2025-11-13 20:23:06 -0600

    commit

--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -7,12 +7,47 @@
 # e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
 # g) Use method chaining to reduce the lines of code within the function.
 
+def reverse_characters(text):
+    characters = list(text)
+    characters.reverse() 
+    reversed_list = ''.join(characters) 
+    return reversed_list 
+
+test_1 = 'apple'
+test_2 = 'LC101'
+test_3 = 'Capitalized Letters'
+test_4 = 'I love the smell of code in the morning.'
+
+print(reverse_characters(test_1))  
+print(reverse_characters(test_2))  
+print(reverse_characters(test_3))  
+print(reverse_characters(test_4)) 
+
+def reverse_characters(text):
+    return ''.join(list(text)[::-1])
 
 
 # 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
 # a) Add an if statement to your reverse_characters function to check the typeof the parameter.
 # b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 # e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
+
+def reverse_characters(values):
+    if isinstance(values, str):
+        return values[::-1] 
+    elif isinstance(values, int):
+        return int(str(values)[::-1]) 
+    elif isinstance(values, float):
+        return float(str(values)[::-1]) 
+    else:
+        return "ERROR: TRY AGAIN"
+
+print(reverse_characters(1234))    
+print(reverse_characters('LC101')) 
+print(reverse_characters(8675309)) 
+print(reverse_characters('radar')) 
+print(reverse_characters(56.89))   
+
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
@@ -22,8 +57,22 @@
 # e) Return the final, reversed list.
 # f) Be sure to print the results from each test case in order to verify your code.
 
+def complete_reversal(old_list):
+    reversed_list = []
+    for value in old_list:
+        reversed_item = reverse_characters(value)
+        reversed_list.insert(0, reversed_item)  
+    return reversed_list
 
 
 list_test1 = ['apple', 'potato', 'Capitalized Words']
 list_test2 = [123, 8897, 42, 1168, 8675309]
 list_test3 = ['hello', 'world', 123, 'orange']
+
+print(complete_reversal(list_test1))  
+print(complete_reversal(list_test2))  
+print(complete_reversal(list_test3))  
+
+
+
+
